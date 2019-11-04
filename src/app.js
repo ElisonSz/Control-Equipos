c76ed3cc7f69d13718a0db48ea3561192b766122
ElisonSz <elisonsanchez15@gmail.com>
2019-11-04 09:50:08 -0600

    se agregaron controladores y servicios para usuarios y equipos; se agrego modelo para funcionamiento de consultas a DB

---
+++ src/app.js
@@ -0,0 +1,33 @@
+/*DEPENDENCIAS*/
+const express = require('express');
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const mysql = require('mysql');
+const myconnetion = require('express-myconnection');
+const dotenv = require('dotenv');
+const path = require('path')
+
+const app = express();
+dotenv.config();
+/* Rutes*/
+
+
+/* midlerwares*/
+app.use(bodyParser.json())
+app.use(morgan('dev'))
+
+/*Conexion DB*/
+app.use(myconnetion(mysql,{
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE}, 'single'));
+
+/*carpetas publicas */
+app.use(express.static(path.join(__dirname,'public')))
+
+app.listen(process.env.PORT||4000, (req,res)=>{
+    console.log('server on port:',process.env.PORT);
+    console.log('http://localhost:',process.env.PORT);
+});
