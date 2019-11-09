4278fb31d7eb484582b1f29fac125a517ce1ff3b
ElisonSz <elisonsanchez15@gmail.com>
2019-11-09 10:03:01 -0600

    se ordeno el codigo}

--- src/controllers/login/login.js
+++ src/controllers/login/login.js
@@ -1,30 +1,30 @@
 const services = require("../../services/login/login")
 
 module.exports = {
-     
-validar : async (req,res)=>{
-     const data = req.body;
-     const user = data.user;
-     const pass = data.pass;
-    
-     if(user && pass){
-         let result = await services.validar(user,pass);
-         if(result.errno){
-             res.status(500).json("Error de servidor")
-         }else if(result.length>0){
-             
-              ID_USUARIO = result[0].ID_USUARIO;
-             ROL = result[0].ROL;
-             ok = true;
-            res.status(200).json({ID_USUARIO , ROL, ok})
-         }else{
-             ok=false
-             res.status(403).json({ok})
-         }
-     }
-},
 
-logout : async (req,res) =>{
+    validar: async (req, res) => {
+        const data = req.body;
+        const user = data.user;
+        const pass = data.pass;
 
-}
+        if (user && pass) {
+            let result = await services.validar(user, pass);
+            if (result.errno) {
+                res.status(500).json("Error de servidor")
+            } else if (result.length > 0) {
+
+                ID_USUARIO = result[0].ID_USUARIO;
+                ROL = result[0].ROL;
+                ok = true;
+                res.status(200).json({ ID_USUARIO, ROL, ok })
+            } else {
+                ok = false
+                res.status(403).json({ ok })
+            }
+        }
+    },
+
+    logout: async (req, res) => {
+
+    }
 }
