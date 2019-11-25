f9a71fa52bac929d958d830365d32dee3c5f35e7
ElisonSz <elisonsanchez15@gmail.com>
2019-11-25 17:34:27 -0600

    se agrego cambios en prstamos

--- src/services/Prestamos/Prestamos.js
+++ src/services/Prestamos/Prestamos.js
@@ -27,6 +27,9 @@ module.exports = {
         return result
     },
 
+
+    
+
     getDataPrestamos : async (data)=>{
         let result;
         try {
@@ -44,7 +47,7 @@ module.exports = {
         let result;
         try {
         await models.withTransaction(db, async ()=>{
-            result = await db.query("SELECT A.ID_PRESTAMO,A.ID_USUARIO,CONCAT(U.NOMBRE,' ',U.APELLIDO) AS NOMBRE,U.CODIGO_EMPLEADO, E.NOMBRE AS NOMBRE_EQUIPO,E.NUMERO_SERIE, A.ESTADO_SALIDA, A.ESTADO_ENTRADA, DATE_FORMAT(A.FECHA_SALIDA,'%m/%d/%Y') AS FECHA_SALIDA, DATE_FORMAT(A.FECHA_ENTRADA, '%m/%d/%Y') AS FECHA_ENTRADA, A.ID_EQUIPO FROM prestamos A INNER JOIN usuarios U ON A.ID_USUARIO=U.ID_USUARIO INNER JOIN equipos E ON A.ID_EQUIPO = E.ID_EQUIPO WHERE A.ESTADO = 1 AND E.ACTIVO = 1 AND U.ACTIVO=1 AND E.DISPONIBLE=1 AND A.FECHA_SALIDA BETWEEN ? AND ?",[fecha_salida,fecha_entrada])
+            result = await db.query("SELECT A.ID_PRESTAMO,A.ID_USUARIO,CONCAT(U.NOMBRE,' ',U.APELLIDO) AS NOMBRE,U.CODIGO_EMPLEADO,U.CORREO AS CORREO_EMPLEADO,U.TELEFONO AS TELEFONO,(SELECT NOMBRE FROM CATEGORIAS WHERE ID_CATEGORIA=E.ID_CATEGORIA) AS CATEGORIA,E.ID_EQUIPO, E.NOMBRE AS NOMBRE_EQUIPO,E.NUMERO_SERIE AS SERIE,E.CARACTERISTICAS, A.ESTADO_SALIDA, A.ESTADO_ENTRADA, DATE_FORMAT(A.FECHA_SALIDA,'%m/%d/%Y') AS FECHA_SALIDA, DATE_FORMAT(A.FECHA_ENTRADA, '%m/%d/%Y') AS FECHA_ENTRADA, A.ID_EQUIPO FROM PRESTAMOS A INNER JOIN USUARIOS U ON A.ID_USUARIO=U.ID_USUARIO INNER JOIN EQUIPOS E ON A.ID_EQUIPO = E.ID_EQUIPO WHERE A.ESTADO = 1 AND E.ACTIVO = 1 AND E.ACTIVO=1 AND E.DISPONIBLE=1 AND A.FECHA_SALIDA BETWEEN ? AND ?",[fecha_salida,fecha_entrada])
         })        
         } catch (err) {
             return err
@@ -54,15 +57,14 @@ module.exports = {
     createPrestamo : async (data)=>{
         let id
         let result
-        let idprestamo
         let idE
-        let idEquipo
+       
         try {
 
         result = await models.withTransaction(db, async ()=>{
         id = await db.query("INSERT INTO prestamos SET ?",[data])
         idprestamo = id.insertId
-    
+            
         idE = result[0].ID_EQUIPO;
         await db.query("UPDATE EQUIPOS SET DISPONIBLE=0 WHERE ID_EQUIPO=?",[idE])
             })
