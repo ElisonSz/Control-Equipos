0154a157416efe4d0bd724bcf7b5ee93779a8c14
Elison Sánchez <51273630+ElisonSz@users.noreply.github.com>
2019-11-30 23:37:14 -0600

    Update Prestamos.js

--- src/routes/Prestamos/Prestamos.js
+++ src/routes/Prestamos/Prestamos.js
@@ -5,6 +5,8 @@ const prestamos = require("../../controllers/Prestamos/Prestamos")
 route.get('/prestamos/all',prestamos.getPrestamo);
 route.get('/prestamos/all/:data',prestamos.getDataPrestamos);
 route.get('/prestamos/:id',prestamos.getIdPrestamo);
+route.get('/prestamos/pendientes',prestamos.getPrestamosPendientes);
+route.get('/prestamos/solventes',prestamos.getPrestamosSolventes);
 route.post('/prestamos',prestamos.createPrestamo);
 route.post('/prestamos/date',prestamos.getPrestamoForDate);
 route.put('/prestamos',prestamos.updatePrestamo);
